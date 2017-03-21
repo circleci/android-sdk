8bdafd1f22c4fe3492b01993a442adf5808bb4e1
Joseph W Becher <drazisil@users.noreply.github.com>
2017-03-21 19:22:31 -0400

    Added missing "

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 echo "Testing for presence of sdkmanager"
 docker run -it $DOCKER_HASH sdkmanager --list
-echo "Testing ability to create AVD with sdcard
+echo "Testing ability to create AVD with sdcard"
 docker run -it $DOCKER_HASH echo "n" | avdmanager -v create avd -n androidlearn -k "system-images;android-25;google_apis;armeabi-v7a" -c 512M -g google_apis  -d "Nexus 7 2013"

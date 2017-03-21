3a8273f17e3fee0ebf461593625d957a43c0891e
Joseph W Becher <drazisil@users.noreply.github.com>
2017-03-21 18:59:48 -0400

    Test creating avd

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ set -e
 set -o pipefail
 
 echo "Testing for presence of sdkmanager"
-docker run -it $DOCKER_HASH sdkmanager --list
+docker run -it $DOCKER_HASH sdkmanager --lis
+echo "Testing ability to create AVD with sdcard
+docker run -it $DOCKER_HASH echo "n" | avdmanager -v create avd -n androidlearn -k "system-images;android-25;google_apis;armeabi-v7a" -c 512M -g google_apis  -d "Nexus 7 2013"

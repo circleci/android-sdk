10634159eaed31f8cd20e61f69ec895f52fdc1b7
Marc O'Morain <github.com@marcomorain.com>
2017-05-30 14:29:23 +0100

    publish

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 if [ $CIRCLE_BRANCH == "master" ]
 then
   echo "Deploying to DockerHub"
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
-  TAG=1.2.$CIRCLE_BUILD_NUM
-  docker push circleci/android-sdk:$TAG
+  docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+  docker push marcomorain/android:$TAG
 else
   echo "Not on master, not deploying"
 fi

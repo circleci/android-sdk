840a6ed0bc45662196122e24c5323eab23fccaf0
Marc O'Morain <github.com@marcomorain.com>
2017-05-30 14:33:06 +0100

    env vars

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ if [ $CIRCLE_BRANCH == "master" ]
 then
   echo "Deploying to DockerHub"
   docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
-  docker push marcomorain/android:$TAG
+  docker push marcomorain/android:0.$CIRCLE_BUILD_NUM
 else
   echo "Not on master, not deploying"
 fi

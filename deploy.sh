a68f9afb4bb2ae535e734fc5b2773e8e9ebba6a7
Marc O'Morain <github.com@marcomorain.com>
2017-06-07 10:30:27 +0100

    Use bundles dockerfile

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 if [ $CIRCLE_BRANCH == "master" ]
 then
   echo "Deploying to DockerHub"
-  docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+  docker login -u "$DOCKERHUB_USER" -p "$DOCKERHUB_PASS"
   docker push marcomorain/android:0.$CIRCLE_BUILD_NUM
 else
   echo "Not on master, not deploying"
